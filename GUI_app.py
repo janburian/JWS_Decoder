bf5fd2c49b6188027bf6a024c1b7b0b0495709ea
janburian <jburian@students.zcu.cz>
2025-02-16 10:16:06 +0100

    Added new icon

--- GUI_app.py
+++ GUI_app.py
@@ -129,7 +129,7 @@ def open_search_dialog():
 # Create the main window
 root = Tk()
 root.title("JWS File Decoder")
-root.iconbitmap(".//icons//icon_ai_2.ico")
+root.iconbitmap(".//icons//icon_ai.ico")
 # root.geometry("1000x500")
 
 window_width = 1000
